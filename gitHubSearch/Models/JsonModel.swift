cdb7def5016a037adc2fe27a233078e9a6844609
Zahidul <z.islam370@gmail.com>
2022-08-30 20:02:01 +0900

    add JSON object  and api service

---
+++ gitHubSearch/Models/JsonModel.swift
@@ -0,0 +1,186 @@
+//
+//  JsonModel.swift
+//  gitHubSearch
+//
+//  Created by Zahidul Islam on 2022/08/30.
+//
+
+import Foundation
+
+protocol JSONDecodable {
+    init(JSON: JSONObject) throws
+}
+
+
+enum JSONDecodeError: Error, CustomDebugStringConvertible {
+    case missingRequiredKey(String)
+    case unexpectedType(key: String, expected: Any.Type, actual: Any.Type)
+    case unexpectedValue(key: String, value: Any, message: String?)
+
+    var debugDescription: String {
+        switch self {
+        case .missingRequiredKey(let key):
+            return "JSON Decode Error: Required key '\(key)' missing"
+        case let .unexpectedType(key: key, expected: expected, actual: actual):
+            return "JSON Decode Error: Unexpected type '\(actual)' was supplied for '\(key): \(expected)'"
+        case let .unexpectedValue(key: key, value: value, message: message):
+            return "JSON Decode Error: \(message ?? "Unexpected value") '\(value)' was supplied for '\(key)'"
+        }
+    }
+}
+
+
+protocol JSONValueConverter {
+    associatedtype FromType
+    associatedtype ToType
+
+    func convert(key: String, value: FromType) throws -> ToType
+}
+
+
+struct DefaultConverter<T>: JSONValueConverter {
+    typealias FromType = T
+    typealias ToType = T
+
+    func convert(key: String, value: FromType) -> DefaultConverter.ToType {
+        return value
+    }
+}
+
+
+struct ObjectConverter<T: JSONDecodable>: JSONValueConverter {
+    typealias FromType = [String: AnyObject]
+    typealias ToType = T
+
+    func convert(key: String, value: FromType) throws -> ObjectConverter.ToType {
+        return try T(JSON: JSONObject(JSON: value))
+    }
+}
+
+
+struct ArrayConverter<T: JSONDecodable>: JSONValueConverter {
+    typealias FromType = [[String: AnyObject]]
+    typealias ToType = [T]
+
+    func convert(key: String, value: FromType) throws -> ArrayConverter.ToType {
+        return try value.map(JSONObject.init).map(T.init)
+    }
+}
+
+
+protocol JSONPrimitive {}
+
+extension String: JSONPrimitive {}
+extension Int: JSONPrimitive {}
+extension Double: JSONPrimitive {}
+extension Bool: JSONPrimitive {}
+
+
+protocol JSONConvertible {
+    associatedtype ConverterType: JSONValueConverter
+    static var converter: ConverterType { get }
+}
+
+
+struct JSONObject {
+
+   
+    let JSON: [String: AnyObject]
+
+    init(JSON: [String: AnyObject]) {
+        self.JSON = JSON
+    }
+
+    func get<Converter: JSONValueConverter>(_ key: String, converter: Converter) throws -> Converter.ToType {
+        guard let value = JSON[key] else {
+            throw JSONDecodeError.missingRequiredKey(key)
+        }
+        guard let typedValue = value as? Converter.FromType else {
+            throw JSONDecodeError.unexpectedType(key: key, expected: Converter.FromType.self, actual: type(of: value))
+        }
+        return try converter.convert(key: key, value: typedValue)
+    }
+
+    func get<Converter: JSONValueConverter>(_ key: String, converter: Converter) throws -> Converter.ToType? {
+        guard let value = JSON[key] else {
+            return nil
+        }
+        if value is NSNull {
+            return nil
+        }
+        guard let typedValue = value as? Converter.FromType else {
+            throw JSONDecodeError.unexpectedType(key: key, expected: Converter.FromType.self, actual: type(of: value))
+        }
+        return try converter.convert(key: key, value: typedValue)
+    }
+
+    func get<T: JSONPrimitive>(_ key: String) throws -> T {
+        return try get(key, converter: DefaultConverter())
+    }
+
+    func get<T: JSONPrimitive>(_ key: String) throws -> T? {
+        return try get(key, converter: DefaultConverter())
+    }
+
+    func get<T: JSONConvertible>(_ key: String) throws -> T where T == T.ConverterType.ToType {
+        return try get(key, converter: T.converter)
+    }
+
+    func get<T: JSONConvertible>(_ key: String) throws -> T? where T == T.ConverterType.ToType {
+        return try get(key, converter: T.converter)
+    }
+
+    func get<T: JSONDecodable>(_ key: String) throws -> T {
+        return try get(key, converter: ObjectConverter())
+    }
+
+    func get<T: JSONDecodable>(_ key: String) throws -> T? {
+        return try get(key, converter: ObjectConverter())
+    }
+
+    func get<T: JSONDecodable>(_ key: String) throws -> [T] {
+        return try get(key, converter: ArrayConverter())
+    }
+
+    func get<T: JSONDecodable>(_ key: String) throws -> [T]? {
+        return try get(key, converter: ArrayConverter())
+    }
+
+}
+
+
+extension URL: JSONConvertible {
+    typealias ConverterType = URLConverter
+    static var converter: ConverterType {
+        return URLConverter()
+    }
+}
+
+extension Date: JSONConvertible {
+    typealias ConverterType = DateConverter
+    static var converter: ConverterType {
+        return DateConverter()
+    }
+}
+
+struct URLConverter: JSONValueConverter {
+    typealias FromType = String
+    typealias ToType = URL
+
+    func convert(key: String, value: FromType) throws -> URLConverter.ToType {
+        guard let URL = URL(string: value) else {
+            throw JSONDecodeError.unexpectedValue(key: key, value: value, message: "Invalid URL")
+        }
+        return URL
+    }
+}
+
+struct DateConverter: JSONValueConverter {
+    typealias FromType = TimeInterval
+    typealias ToType = Date
+
+    func convert(key: String, value: FromType) -> DateConverter.ToType {
+        return Date(timeIntervalSince1970: value)
+    }
+}
+
---
+++ gitHubSearch/Services/ApiService.swift
@@ -0,0 +1,100 @@
+//
+//  ApiService.swift
+//  gitHubSearch
+//
+//  Created by Zahidul Islam on 2022/08/30.
+//
+
+
+import Foundation
+
+extension URL {
+    static let searchRepoURLString: String = "https://api.github.com/search/repositories?"
+    static let sortString: String = "&sort=stars&order=desc"
+}
+
+enum HTTPMethod: String {
+    case GET
+}
+
+enum APIError: Error {
+    case emptyBody
+    case unexpectedResponseType
+}
+
+enum APIResult {
+    case success(SearchResult)
+    case failure(Error)
+}
+
+struct SearchResult: JSONDecodable {
+    let items: [Repository]
+
+    init(JSON: JSONObject) throws {
+        self.items = try JSON.get("items")
+    }
+}
+
+struct Repository: JSONDecodable {
+    let htmlUrl: URL
+    let fullName: String
+    let language: String?
+    let stargazersCount: Int
+
+    init(JSON: JSONObject) throws {
+        self.htmlUrl = try JSON.get("html_url")
+        self.fullName = try JSON.get("full_name")
+        self.language = try JSON.get("language")
+        self.stargazersCount = try JSON.get("stargazers_count")
+    }
+}
+
+protocol Endpoint {
+    var url: URL { get }
+    var method: HTTPMethod { get }
+    var query: String { get }
+}
+
+extension Endpoint {
+    fileprivate var urlRequest: URLRequest {
+        var req = URLRequest(url: url)
+        req.httpMethod = method.rawValue
+        return req
+    }
+}
+
+class SearchRepository: Endpoint {
+    var url: URL
+    var method: HTTPMethod
+    var query: String
+
+    init(query: String) {
+        guard let url = URL(string: URL.searchRepoURLString + "q=\(query)" + URL.sortString) else { fatalError("Could not configure URL") }
+        self.url = url
+        self.method = .GET
+        self.query = query
+    }
+
+    func request(callback: @escaping (APIResult) -> Void) {
+        URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            if let e = error {
+                callback(.failure(e))
+            } else if let data = data {
+                do {
+                    guard let dic = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
+                        throw APIError.unexpectedResponseType
+                    }
+                    let jsonObject = JSONObject(JSON: dic)
+                    let response = try SearchResult(JSON: jsonObject)
+                    callback(.success(response))
+                } catch {
+                    callback(.failure(error))
+                }
+            } else {
+                callback(.failure(APIError.emptyBody))
+            }
+        }).resume()
+    }
+}
+
+
